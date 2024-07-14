1cc7a2f46b74cef1dc6fe52dce8cab3aebaf5c77
bipproduction <bip.production.js@gmail.com>
2024-07-15 06:28:52 +0800

    perbaikan di height chat yang tidak 100%

--- src/ui/Chat.tsx
+++ src/ui/Chat.tsx
@@ -84,127 +84,126 @@ export function Chat({ apiKey }: { apiKey: string }) {
     scrollToBottom();
   };
   return (
-    <Stack pos={"fixed"} w={"100%"} h={"100%"} bg={"dark"}>
-      <Grid w={"100%"} h={"100vh"} gutter={0} pos={"relative"}>
-        <Grid.Col
-          miw={300}
-          span={"content"}
-          bg={colors["bg-header"]}
-          visibleFrom="md"
+    <Grid pos={"fixed"} w={"100%"} h={"100%"} bg={"dark"} gutter={0}>
+      <Grid.Col
+        miw={300}
+        h={"100vh"}
+        span={"content"}
+        bg={colors["bg-header"]}
+        visibleFrom="md"
+      >
+        <Stack
+          h={"100vh"}
+          p={"md"}
+          gap={0}
+          style={{
+            overflowX: "scroll",
+          }}
         >
-          <Stack
-            h={"100vh"}
-            p={"md"}
-            gap={0}
-            style={{
-              overflowX: "scroll",
-            }}
-          >
-            {listChat
-              .filter((v) => v.role !== "system")
-              .map((v, k) => (
-                <NavLink
-                  key={k}
-                  label={
-                    <Flex gap={"md"} align={"center"}>
-                      <Avatar>{v.role === "user" ? "Ai" : "Me"}</Avatar>
-                      <Stack gap={0}>
-                        <Text>{v.role}</Text>
-                        <Text c={"gray"} fz={"sm"}>
-                          {v.content.substring(0, 20)}
-                        </Text>
-                      </Stack>
-                    </Flex>
-                  }
-                />
-              ))}
-          </Stack>
-        </Grid.Col>
-        <Grid.Col span={"auto"} bg={colors["bg-chat"]} pos={"relative"}>
-          <Flex
-            h={70}
-            align={"center"}
-            justify={"space-between"}
-            px={"md"}
-            bg={colors["bg-header"]}
-          >
-            <Flex>
-              <Avatar>Ai</Avatar>
-            </Flex>
-            <Flex gap={"md"}>
-              <ActionIcon onClick={onCleanAll} variant="transparent">
-                <MdClearAll size={"1.5rem"} />
-              </ActionIcon>
-              <AppDotMenu />
-            </Flex>
+          {listChat
+            .filter((v) => v.role !== "system")
+            .map((v, k) => (
+              <NavLink
+                key={k}
+                label={
+                  <Flex gap={"md"} align={"center"}>
+                    <Avatar>{v.role === "user" ? "Ai" : "Me"}</Avatar>
+                    <Stack gap={0}>
+                      <Text>{v.role}</Text>
+                      <Text c={"gray"} fz={"sm"}>
+                        {v.content.substring(0, 20)}
+                      </Text>
+                    </Stack>
+                  </Flex>
+                }
+              />
+            ))}
+        </Stack>
+      </Grid.Col>
+      <Grid.Col span={"auto"} bg={colors["bg-chat"]} pos={"relative"} h={"100vh"}>
+        <Flex
+          h={70}
+          align={"center"}
+          justify={"space-between"}
+          px={"md"}
+          bg={colors["bg-header"]}
+        >
+          <Flex>
+            <Avatar>Ai</Avatar>
           </Flex>
-          <Stack
-            ref={ref}
-            h={"80vh"}
-            p={"md"}
-            style={{
-              overflowY: "auto",
-            }}
-          >
-            {listChat
-              .filter((v) => v.role !== "system")
-              .map((v, k) => (
-                <Flex
-                  w={"100%"}
-                  key={k}
-                  gap={0}
-                  justify={v.role === "user" ? "flex-end" : "flex-start"}
-                >
-                  <Paper
-                    withBorder
-                    shadow="xs"
-                    px={"sm"}
-                    pos={"relative"}
-                    bg={
-                      v.role === "user"
-                        ? colors["bg-booble-me"]
-                        : colors["bg-booble-you"]
-                    }
-                    style={{
-                      maxWidth: "80%",
-                      borderRadius: "20px",
-                    }}
-                  >
-                    <MarkdownRender markdown={v.content} />
-                  </Paper>
-                </Flex>
-              ))}
-          </Stack>
-          <Flex
-            p={"sm"}
-            bg={colors["bg-header"]}
-            pos={"absolute"}
-            bottom={0}
-            w={"100%"}
-            gap={"md"}
-            align={"center"}
-            justify={"space-between"}
-          >
-            <TextInput
-              onKeyDown={(e) => {
-                if (e.key === "Enter") onKirim();
-              }}
-              value={pesan}
-              onChange={(e) => setPesan(e.target.value)}
-              size="md"
-              w={"100%"}
-              placeholder="Tulis sesuatu..."
-            />
-            <ActionIcon
-              loading={loadingPesan}
-              variant="transparent"
-              onClick={onKirim}
-            >
-              <MdSend size={"2rem"} />
+          <Flex gap={"md"}>
+            <ActionIcon onClick={onCleanAll} variant="transparent">
+              <MdClearAll size={"1.5rem"} />
             </ActionIcon>
+            <AppDotMenu />
           </Flex>
-        </Grid.Col>
-      </Grid>
-    </Stack>
+        </Flex>
+        <Stack
+          ref={ref}
+          h={"80vh"}
+          p={"md"}
+          style={{
+            overflowY: "auto",
+          }}
+        >
+          {listChat
+            .filter((v) => v.role !== "system")
+            .map((v, k) => (
+              <Flex
+                w={"100%"}
+                key={k}
+                gap={0}
+                justify={v.role === "user" ? "flex-end" : "flex-start"}
+              >
+                <Paper
+                  withBorder
+                  shadow="xs"
+                  px={"sm"}
+                  pos={"relative"}
+                  bg={
+                    v.role === "user"
+                      ? colors["bg-booble-me"]
+                      : colors["bg-booble-you"]
+                  }
+                  style={{
+                    maxWidth: "80%",
+                    borderRadius: "20px",
+                  }}
+                >
+                  <MarkdownRender markdown={v.content} />
+                </Paper>
+              </Flex>
+            ))}
+        </Stack>
+        <Flex
+          p={"sm"}
+          bg={colors["bg-header"]}
+          pos={"absolute"}
+          bottom={0}
+          w={"100%"}
+          gap={"md"}
+          align={"center"}
+          justify={"space-between"}
+        >
+          <TextInput
+            onKeyDown={(e) => {
+              if (e.key === "Enter") onKirim();
+            }}
+            value={pesan}
+            onChange={(e) => setPesan(e.target.value)}
+            size="md"
+            w={"100%"}
+            placeholder="Tulis sesuatu..."
+          />
+          <ActionIcon
+            loading={loadingPesan}
+            variant="transparent"
+            onClick={onKirim}
+          >
+            <MdSend size={"2rem"} />
+          </ActionIcon>
+        </Flex>
+      </Grid.Col>
+    </Grid>
   );
 }
